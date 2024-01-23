5c09e256486edcbf006248b5f4ae2eb74cb9b671
Bart <bartoszkmiecik@katanaus.com>
2024-01-23 08:04:42 +0100

    Creating multi line module and starting it in blender

--- MultipleFileTest/__init__.py
+++ MultipleFileTest/__init__.py
@@ -1,78 +1,185 @@
-#----------------------------------------------------------
-# File __init__.py
-#----------------------------------------------------------
-#
-# The user interface is created in the "right" panel in the 3D View
-# This panel is normally closed, so it must be opened (+) to be seen.
-#
- 
-#    Addon info
+import glob, sys, time, math, random, asyncio, bpy, os
+from bpy.props import EnumProperty
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy.utils import register_class, unregister_class
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from . import utility
+# import test_package
+# import logging
+# pkg_dir = "D:\\BlenderAddons\\Maker\\testpackage\\utilit_material" 
+# sys.path.append(pkg_dir)
+# print(f'AAAAAAAAAAAA path: {sys.path}')
+
 bl_info = {
-  "name": "Multifile",
-  'author': 'Based on work by Thomas Larsson',
-  "location": "View3D &gt; UI panel &gt; Add meshes",
-   "category": "3D View"
-  }
+	'name': 'BARTOS',
+	'description': 'Various tools for handle geodata',
+	'author': 'BARTOS',
+	'license': 'GPL',
+	'deps': '',
+	'version': (2, 2, 8),
+	'blender': (2, 83, 0),
+	'location': 'View3D > Tools > BARTOS',
+	'warning': '',
+	'wiki_url': 'https://github.com/BARTOS/BlenderGIS/wiki',
+	'tracker_url': 'https://github.com/BARTOS/BlenderGIS/issues',
+	'link': '',
+	'support': 'COMMUNITY',
+	'category': '3D View'
+}
 
-# To support reload properly, try to access a package var, 
-# if it's there, reload everything
-if "bpy" in locals():
-  import imp
-  imp.reload(mycube)
-  imp.reload(mysphere)
-  imp.reload(mycylinder)
-  print("Reloaded multifiles")
-else:
-  import mycube, mysphere, mycylinder
-  print("Imported multifiles")
+class TEST_OT_test_op(Operator, AddObjectHelper):
+    bl_idname = 'test.test_op'
+    bl_label = 'Test'
+    bl_description = 'Test'
+    bl_options = {'REGISTER', 'UNDO'}
 
-import bpy
-from bpy.props import *
 
-#
-#   class AddMeshPanel(bpy.types.Panel):
-#
-class AddMeshPanel(bpy.types.Panel):
-  bl_label = "Add meshes"
-  bl_space_type = "VIEW_3D"
-  bl_region_type = "UI"
+    object_to_hide = []
+    car_meshes = {}
+    action: EnumProperty(
+        items=[
+            ('ADD_CUBE', 'add cube', 'add cube'),
+            ('REMOVE_CUBE', 'remove object', 'remove object'),
+            ("RANDOM_MOVE", 'move object', 'move object'),
+            ("DIFF_MATERIAL", 'diff mat', 'diff mat'),
+            ("ABC_IMPORT", 'abc import', 'abc import'),
+            ("ABC_ASIMPORT", 'abc asimport', 'abc asimport'),
+            ("GET_ALL", 'get all', 'get all'),
+            ("HIDE_RANDOM", 'hide random', 'hide random'),
+            ("UNHIDE_RANDOM", 'unhide random', 'unhide random'),
+            ("SAVE_SCENE", 'save scene', 'save scene'),
+    ]
+)
 
-  def draw(self, context):
-    self.layout.operator("multifile.add", text="Add cube").mesh = "cube"
-    self.layout.operator("multifile.add", text="Add cylinder").mesh = "cylinder"
-    self.layout.operator("multifile.add", text="Add sphere").mesh = "sphere"
+    def execute(self, context):
+        if self.action == 'ADD_CUBE':
+            self.add_cube(context=context)
+        elif self.action == 'REMOVE_CUBE':
+            _obj = bpy.context.selected_objects
+            self.remove_object(context=context, obj_to_remove=_obj)
+        elif self.action == 'RANDOM_MOVE':
+            _obj = bpy.context.selected_objects
+            self.randomMove(context, _obj)
+        elif self.action == 'DIFF_MATERIAL':
+            _obj = bpy.context.object
+            self.diffMate(context, _obj)
+        elif self.action == 'ABC_IMPORT':
+            self.abc_import_synconus(self, context)
+        elif self.action == 'ABC_ASIMPORT':
+            asyncio.run(self.abc_import_assync(context))
+        elif self.action == 'GET_ALL':
+            self.get_all(self)
+        elif self.action == 'HIDE_RANDOM':
+            self.hide_random_third_of_car(self, context)
+        elif self.action == 'UNHIDE_RANDOM':
+            self.unhide_random_third_of_car(self, context)
+        elif self.action == 'SAVE_SCENE':
+            self.save_scene(context)
+        return {'FINISHED'}
+ 
+    @staticmethod
+    def remove_object(context, obj_to_remove):
+        utility.remove_object(context, obj_to_remove)
+ 
+    @staticmethod
+    def add_cube(context):
+        utility.add_cube(context)
+ 
+    
+    @staticmethod
+    def randomMove(context, cube):
+        utility.randomMove(context, cube)
+    
+    @staticmethod
+    def diffMate(context, cube):
+        utility.diffMate(context, cube)
+    
+    @staticmethod
+    def abc_import_synconus(self, context):
+        utility.abc_import_synconus(self, context)
 
-#
-#   class OBJECT_OT_AddButton(bpy.types.Operator):
-#
-class OBJECT_OT_AddButton(bpy.types.Operator):
-  bl_idname = "multifile.add"
-  bl_label = "Add"
-  mesh = bpy.props.StringProperty()
+    @staticmethod
+    async def abc_import_assync(context):
+        utility.abc_import_assync(context)
+    
 
-  def execute(self, context):
-    if self.mesh == "cube":
-      mycube.makeMesh(-8)
-      print ("Added a cube")
-    elif self.mesh == "cylinder":
-      mycylinder.makeMesh(-5)
-      print ("Added a cylinder")
-    elif self.mesh == "sphere":
-      mysphere.makeMesh(-2)
-      print ("Added a sphere")
-    return{'FINISHED'}    
+    def get_all(self,context):
+        utility.get_all(self, context)
 
-#
-#    Registration
-#
+    def get_random_car_part(self):
+        rand_obj = utility.get_random_car_part(self)
+        return rand_obj
+
+    @staticmethod
+    def hide_random_third_of_car(self, context):
+        utility.hide_random_third_of_car(self, context)
+        
+    @staticmethod
+    def unhide_random_third_of_car(self, context):
+        utility.unhide_random_third_of_car(self, context)
+        
+
+    @staticmethod
+    def save_scene(context, filepath = 'D:\BlenderAddons\Scenes\Test'):
+        utility.save_scene(context, filepath)
+        
+
+
+class Test_Panel(bpy.types.Panel):
+    bl_label = "Test Panel"
+    bl_idname = "PT_TestPanel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'NewTestTab'
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        row = layout.row()
+        row.label(text="Spawn Cube", icon='CUBE')
+        row = layout.row()
+        row.operator('test.test_op', text='Add Cube').action = 'ADD_CUBE'
+        row = layout.row()
+        row.operator('test.test_op', text='Remove Cube').action = 'REMOVE_CUBE'
+        row = layout.row()
+        row.operator('test.test_op', text='Move').action = 'RANDOM_MOVE'
+        row = layout.row()
+        row.operator('test.test_op', text='Random material').action = 'DIFF_MATERIAL'
+        row = layout.row()
+        row.operator('test.test_op', text='Import sync alembics').action = 'ABC_IMPORT'
+        row = layout.row()
+        row.operator('test.test_op', text='Import async alembics (NOT WORKING YET)').action = 'ABC_ASIMPORT'
+        row = layout.row()
+        row.operator('test.test_op', text='Get all').action = 'GET_ALL'
+        row = layout.row()
+        row.operator('test.test_op', text='Hide random').action = 'HIDE_RANDOM'
+        row = layout.row()
+        row.operator('test.test_op', text='Unhide all').action = 'UNHIDE_RANDOM'
+        row = layout.row()
+        row.operator('test.test_op', text='Save scene').action = 'SAVE_SCENE'
+
+# def add_object_manual_map():
+#    url_manual_prefix = "https://docs.blender.org/manual/en/latest/BARTOS"
+#    url_manual_mapping = (
+#        ("bpy.ops.mesh.add_object", "scene_layout/object/BARTOS/types.html"),
+#    )
+#    return url_manual_prefix, url_manual_mapping
 
 def register():
-  print ("Registering ", __name__)
-  bpy.utils.register_module(__name__)
+    register_class(TEST_OT_test_op)
+    register_class(Test_Panel)
+    # bpy.utils.register_manual_map(add_object_manual_map)
+
 
 def unregister():
-  print ("Unregistering ", __name__)
-  bpy.utils.unregister_module(__name__)
+    unregister_class(TEST_OT_test_op)
+    register_class(Test_Panel)
+    # bpy.utils.register_manual_map(add_object_manual_map)
+
+
+# register()
+
 
 if __name__ == "__main__":
-  register()
+	register()
---
+++ MultipleFileTest/utility.py
@@ -0,0 +1,163 @@
+import glob, sys, time, math, random, asyncio, bpy, os
+
+object_to_hide = []
+car_meshes = {}
+
+def remove_object(context, obj_to_remove):
+    if obj_to_remove == None:
+        obj_to_remove = bpy.context.selected_objects
+    if obj_to_remove != None:
+        bpy.ops.object.delete()
+
+
+def add_cube(context):
+    # bpy.ops.mesh.primitive_cube_add()
+    cube = bpy.ops.mesh.primitive_cube_add(size=4, location=(0, 0, 0))
+    new_mat = bpy.data.materials.new(name="Material")
+    new_mat.use_nodes = True
+    bsdf = new_mat.node_tree.nodes["Principled BSDF"]
+    color_ramp = new_mat.node_tree.nodes.new("ShaderNodeValToRGB")
+    color_ramp.color_ramp.elements[0].color = (1.0, 1.0, 1.0, 1)
+    new_mat.node_tree.links.new(bsdf.inputs['Base Color'], color_ramp.outputs['Color'])
+
+    cube = bpy.context.selected_objects
+    for obj in cube:
+        obj.data.materials.append(new_mat)
+
+
+
+def randomMove(context, cube):
+    if cube is None or cube == []:
+        cube = bpy.context.selected_objects
+    if cube is not None:
+        x = random.randint(-10, 10)
+        y = random.randint(-10, 10)
+        z = random.randint(-10, 10)
+        cube[0].location = (x, y, z)
+
+
+def diffMate(context, cube):
+    if cube is None or cube is []:
+        cube = bpy.context.object
+        print(cube)
+    if cube is not None:
+        try:
+            material_basic = bpy.data.materials['Basic Mat']        
+        except:
+            material_basic = bpy.data.materials.new(name='Basic Mat')
+        material_basic.use_nodes = True
+        principled_node = material_basic.node_tree.nodes.get('Principled BSDF')
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        m = random.random()
+        roug = random.random()
+        ior = random.random() * random.randint(1, 3)
+        # print(f'Ref:{r}, Blue:{b}, green: {g}')
+        principled_node.inputs[0].default_value = (r, g, b, 1)
+        principled_node.inputs[1].default_value = m
+        principled_node.inputs[2].default_value = roug
+        principled_node.inputs[3].default_value = ior
+        textur_node = material_basic.node_tree.nodes.get('ShaderNodeTexImage')
+        if textur_node == None:
+            textur_node = material_basic.node_tree.nodes.new('ShaderNodeTexImage')
+        textur_node.location = (-380, 300)
+        images_list = list(glob.glob("D:\BlenderAddons\Maker\Images\*"))
+        random_image = images_list[random.randint(0, len(images_list)-1)]
+        textur_node.image = bpy.data.images.load(random_image)
+        material_basic.node_tree.links.new(textur_node.outputs[0], principled_node.inputs[0])
+        cube.active_material = material_basic
+
+def abc_import_synconus(self, context):
+    abc_list = list(glob.glob("D:\\alembicCar\\*"))
+    t_start = time.time()
+    for n in abc_list:
+        bpy.ops.wm.alembic_import(filepath=n, relative_path=True, as_background_job=False)
+        try:
+            tem_name = os.path.basename(n)
+            obj = bpy.context.object
+            obj.name = tem_name
+            self.car_meshes[obj] = True
+        except:
+            print('Missing mesh')
+    t_end = time.time()
+
+    print(f'Total importing time: {t_end - t_start}')
+
+async def abc_import_assync(context):
+    abc_list = list(glob.glob("D:\\alembicCar\\*"))
+    for n in abc_list:
+        # print(f'Importing: {n}')
+        batch = asyncio.gather(bpy.ops.wm.alembic_import(filepath=n, relative_path=True, as_background_job=True))
+        resu = await batch
+        try:
+            tem_name = os.path.basename(n)
+            obj = bpy.context.object
+            obj.name = tem_name
+        except:
+            print('Missing mesh')
+
+def get_all(self,context):
+    t_start = time.time()
+    self.car_meshes.clear()
+    for ob in bpy.data.objects:
+        # print (ob.name)
+        # print(f'Obj: {ob.name} is vis: {not bpy.data.objects[ob.name].hide_viewport}')
+        self.car_meshes[ob.name] = not bpy.data.objects[ob.name].hide_viewport
+    t_end = time.time()
+    print(f'Select all objects in scene: {t_end - t_start}')
+
+def get_random_car_part(self):
+    avaliable_list = []
+    for key, value in self.car_meshes.items():
+        if value:
+            avaliable_list.append(key)
+    
+    rand_int = random.randint(0, len(avaliable_list)-1)
+    rand_obj = avaliable_list[rand_int]
+    self.car_meshes[rand_obj] = False
+    return rand_obj
+
+
+def hide_random_third_of_car(self, context):
+    t_start = time.time()
+    if len(self.object_to_hide) > 0:
+        self.unhide_random_third_of_car(self, context)
+    one_third = len(self.car_meshes) // 3
+    for i in range(one_third):
+        temp = self.get_random_car_part()
+        self.object_to_hide.append(temp)
+        bpy.data.objects[temp].hide_viewport = True
+    t_end = time.time()
+    print(f'Hide random one/third: {t_end - t_start}')
+    
+
+def unhide_random_third_of_car(self, context):
+    if len(self.car_meshes.items()) == 0:
+        self.get_all(context)
+    t_start = time.time()
+    for key, value in self.car_meshes.items():
+        if not value:
+            bpy.data.objects[key].hide_viewport = False
+            self.car_meshes[key] = True
+    self.object_to_hide.clear()
+    t_end = time.time()
+    print(f'Unhide random one/third: {t_end - t_start}')
+    
+
+
+def save_scene(context, filepath = 'D:\BlenderAddons\Scenes\Test'):
+    t_start = time.time()
+
+    extension = '.blend'
+    filename = filepath
+    # print(f'File name: {filename}, extension: {extension} AAAAAAAAAAAAAAAAAAAAAA')
+    counter = 1
+    filepath = f'{filepath}{extension}'
+    while os.path.exists(filepath):
+        filepath = f'{filename}({counter}){extension}'
+        counter += 1
+
+    bpy.ops.wm.save_as_mainfile(filepath=f'{filepath}')
+    t_end = time.time()
+    print(f'Scene saved to: {filepath}, took: {t_end - t_start} sec')
